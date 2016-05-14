72a4742dba021397847df0d9ebc38b5b38618baf
Jack Bush <bush.jack@gmail.com>
2016-05-14 15:06:13 +0100

    basic phone frame and page dynamic working

--- js/dist/main.output.js
+++ js/dist/main.output.js
@@ -1,6 +1,15 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"/Users/jackbush/Repos/breathe-app/js/src/main.js":[function(require,module,exports){
-window.jq = require('jquery');
+var $ = window.jq = require('jquery');
 
+var elHomepage = $('.js-homepage');
+var elToggleHomepage = $('.js-toggle-homepage');
+
+function toggleHomepage () {
+	console.log('hi!');
+	elHomepage.toggleClass('active');
+}
+
+elToggleHomepage.on('click', toggleHomepage);
 
 },{"jquery":"/Users/jackbush/Repos/breathe-app/node_modules/jquery/dist/jquery.js"}],"/Users/jackbush/Repos/breathe-app/node_modules/jquery/dist/jquery.js":[function(require,module,exports){
 /*!
--- js/src/main.js
+++ js/src/main.js
@@ -1,2 +1,11 @@
-window.jq = require('jquery');
+var $ = window.jq = require('jquery');
 
+var elHomepage = $('.js-homepage');
+var elToggleHomepage = $('.js-toggle-homepage');
+
+function toggleHomepage () {
+	console.log('hi!');
+	elHomepage.toggleClass('active');
+}
+
+elToggleHomepage.on('click', toggleHomepage);
